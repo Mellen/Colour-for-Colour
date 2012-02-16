7ff591ddf6c8fbda644a61ab8a0847393a8e87ec
Matthew Ellen <matt@matthewellen.co.uk>
2012-02-16 19:47:20 +0000

    updated the readme to include installation instructions created a bat file to move the files to the right place

---
+++ ColourForColour/install.bat
@@ -0,0 +1,5 @@
+md %localappdata%\Microsoft\VisualStudio\10.0\Extensions\Mellen\ColourForColour
+copy ColourForColour.dll %localappdata%\Microsoft\VisualStudio\10.0\Extensions\Mellen\ColourForColour\ColourForColour.dll
+copy extension.vsixmanifest %localappdata%\Microsoft\VisualStudio\10.0\Extensions\Mellen\ColourForColour\extension.vsixmanifest
+copy CFC.ico %localappdata%\Microsoft\VisualStudio\10.0\Extensions\Mellen\ColourForColour\CFC.ico
+copy CFCPreview.gif %localappdata%\Microsoft\VisualStudio\10.0\Extensions\Mellen\ColourForColour\CFCPreview.gif
